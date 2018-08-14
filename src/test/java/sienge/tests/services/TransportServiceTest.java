26339c1a3aee6eca74976abfd210d04dd6496eac
Tiago Dakuzaku <tdakuzaku@gmail.com>
2018-08-14 09:56:47 -0300

    Added more tests and wtf's

--- src/test/java/sienge/tests/services/TransportServiceTest.java
+++ src/test/java/sienge/tests/services/TransportServiceTest.java
@@ -25,6 +25,9 @@ public class TransportServiceTest {
 
     private final Vehicle v1 = new Vehicle("bucket_truck", 8);
     private final Vehicle v2 = new Vehicle("truck_trunk", 4);
+    private final Vehicle v3 = new Vehicle("cart", 12);
+    private final Vehicle v4 = new Vehicle("truck_trunk", 6);
+    private final Vehicle v5 = new Vehicle("bucket_truck", 5);
     private final Transport t1 = new Transport();
     private final TransportService ts1 = new TransportService();
     
@@ -40,5 +43,26 @@ public class TransportServiceTest {
     	RequestWrapper req = new RequestWrapper(v2, t1, 0f, 60f);
         assertThat(ts1.shippingCalculation(req))
             .isEqualTo(37.2f);
+    }
+    
+    @Test
+    public void checkEx3Shipping() {
+    	RequestWrapper req = new RequestWrapper(v3, t1, 0f, 180f);
+        assertThat(ts1.shippingCalculation(req))
+            .isEqualTo(150.192f); //wtf
+    }
+    
+    @Test
+    public void checkEx4Shipping() {
+    	RequestWrapper req = new RequestWrapper(v4, t1, 80f, 20f);
+        assertThat(ts1.shippingCalculation(req))
+            .isEqualTo(57.60f);
+    }
+    
+    @Test
+    public void checkEx5Shipping() {
+    	RequestWrapper req = new RequestWrapper(v5, t1, 50f, 30f);
+        assertThat(ts1.shippingCalculation(req))
+            .isEqualTo(47.88f);
     }
 }
